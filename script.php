42d32ba2722c593afe7603513830c324a2b93093
trevorewillz <133253596+trevorewillz@users.noreply.github.com>
2023-05-26 08:11:52 +0100

    Create script.php

---
+++ script.php
@@ -0,0 +1,14 @@
+ <?php
+
+$name = "Offiah Godswill Uche";
+$regnumber = "2021/HND/33791/CS";
+$soo = "Abia State";
+$topic = "Online Students Complain System. A case study of Computer Science, AIFPU";
+
+
+echo "Name: $name<br>";
+echo "Regnumber: $regnumber<br>";
+echo "State of origin: $soo<br>";
+echo "Project topic: $topic<br>";
+
+?>
